26203b50df5b91888ec2e17a1e6a84a3c75cd280
saadalajlan23 <48617522+saadalajlan23@users.noreply.github.com>
2021-10-31 11:49:57 +0000

    Initial Commit

---
+++ Messenger-AppTests/Messenger_AppTests.swift
@@ -0,0 +1,33 @@
+//
+//  Messenger_AppTests.swift
+//  Messenger-AppTests
+//
+//  Created by administrator on 31/10/2021.
+//
+
+import XCTest
+@testable import Messenger_App
+
+class Messenger_AppTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
